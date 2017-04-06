de2a89495869a1bf5cc2fc3289cf5fe9c7b4804a
zhangguanyu02 <zhangguanyu02@meituan.com>
2017-04-06 17:08:17 +0800

    使用模板独立出刷新按钮和feedback

--- pages/function/achievement/achievement.js
+++ pages/function/achievement/achievement.js
@@ -1,23 +1,37 @@
-var {showToastError,fetchAchievement}=require("../../../utils/wx");
+var { showToastError, fetchAchievement } = require("../../../utils/wx");
 
 Page({
     data: {
-        achievementList:[]
+        //目前没有成绩模拟下
+        achievementList: [{
+            kch: "课程号",
+            kxh: "课序号",
+            kcm: "课程名",
+            kcywm: "课程英文名",
+            xf: "学分",
+            kcsx: "课程属性",
+            cj: "成绩"
+        }],
+        refreshDisabled: false
     },
     onLoad: function(options) {
-        var achievementList=wx.getStorageSync('achievementList');
-        if(achievementList){
+        var achievementList = wx.getStorageSync('achievementList');
+        achievementList = JSON.parse(achievementList);
+        if (achievementList.length) {
             this.setData({
-                achievementList:JSON.parse(achievementList)
+                achievementList
             });
-        }else{
+        } else {
             this.fetch();
         }
     },
-    fetch(){
-        var _this=this;
-        fetchAchievement("cache").then((achievementList)=>{
-            _this.setData({achievementList});
+    refresh() {
+
+    },
+    fetch() {
+        var _this = this;
+        fetchAchievement("cache").then((achievementList) => {
+            _this.setData({ achievementList });
             wx.setStorage({
                 key: 'achievementList',
                 data: JSON.stringify(achievementList)

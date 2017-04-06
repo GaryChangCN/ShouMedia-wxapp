7716c5baeebc2bfaab079452dc05f15639620647
garychangcn <garychangcn@hotmail.com>
2017-04-06 21:28:51 +0800

    切换config

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // url:"http://192.168.0.106:7777"
-    url: "https://f79.r.fe.dev.sankuai.com"
+    url:"http://192.168.0.106:7777"
+    // url: "https://f79.r.fe.dev.sankuai.com"
 }

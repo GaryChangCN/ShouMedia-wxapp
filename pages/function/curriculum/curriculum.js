309f0fae1bd21341abccda74b393a84aebf79e17
zhangguanyu02 <zhangguanyu02@meituan.com>
2017-04-06 16:25:28 +0800

    添加课程表刷新以及缓存

--- pages/function/curriculum/curriculum.js
+++ pages/function/curriculum/curriculum.js
@@ -1,25 +1,55 @@
-var {showToastError,fetchCurriculum}=require("../../../utils/wx");
+var { showToastError, fetchCurriculum } = require("../../../utils/wx");
 
 Page({
     data: {
-        curriculumList:[[]],
-        day:""
-    },
-    onLoad: function(options) {
-        var _this=this;
-        fetchCurriculum("cache").then((curriculumList)=>{
-            _this.setData({
-                curriculumList
-            });
-        }).catch((err)=>{
-            showToastError(err);
-        });
+        curriculumList: [
+            []
+        ],
+        day: "",
+        animationData: "",
+        rotate: 0,
+        refreshDisabled: false
     },
     onReady: function() {
-        var day=new Date().getDay();
-        day=day===0?6:day-1;
+        var day = new Date().getDay();
+        day = day === 0 ? 6 : day - 1;
         this.setData({
             day
+        });
+    },
+    onLoad: function(options) {
+        this.fetch("cache");
+    },
+    refresh() {
+        var animation = wx.createAnimation({
+            duration: 1000,
+        });
+        var { rotate } = this.data;
+        rotate += 720;
+        animation.rotate(rotate).step();
+        this.setData({
+            animationData: animation.export(),
+            rotate,
+            refreshDisabled: true
+        });
+        if (rotate === 720 || rotate === 3600) {
+            this.fetch("fresh");
+        } else {
+            this.fetch("cache");
+        }
+    },
+    fetch(type) {
+        var _this = this;
+        this.setData({
+            refreshDisabled: true
+        })
+        fetchCurriculum(type).then((curriculumList) => {
+            _this.setData({
+                curriculumList,
+                refreshDisabled: false
+            });
+        }).catch((err) => {
+            showToastError(err);
         });
     }
 })
--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,4 @@
-// module.exports="https://weixin.shouedu.cn"
-// module.exports="https://192.168.0.106"
-module.exports={
-    url:"http://192.168.0.106:7777"
+module.exports = {
+    // url:"http://192.168.0.106:7777"
+    url: "https://f79.r.fe.dev.sankuai.com"
 }

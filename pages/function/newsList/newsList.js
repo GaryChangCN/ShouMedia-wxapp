ac22d9770a99934f774475a4252203d3bbb23d56
garychangcn <garychangcn@hotmail.com>
2017-04-09 00:30:44 +0800

    修复低版本微信不支持showLoading

--- pages/function/newsList/newsList.js
+++ pages/function/newsList/newsList.js
@@ -1,59 +1,58 @@
-var {fetchNewsList,showToastError}=require("../../../utils/wx");
+var { fetchNewsList, showToastError } = require("../../../utils/wx");
 
 Page({
     data: {
-        tab:"tzgg",
-        pn:1,
-        list:{
-            tzgg:[],
-            yw:[],
-            mtjj:[],
-            xsqy:[],
-            xsjz:[]
+        tab: "tzgg",
+        pn: 1,
+        list: {
+            tzgg: [],
+            yw: [],
+            mtjj: [],
+            xsqy: [],
+            xsjz: []
         },
-        fetching:false
+        fetching: false
     },
     onLoad: function(options) {
         this.fetch();
-        
+
     },
-    changeTab(e){
-        var {type}=e.currentTarget.dataset
+    changeTab(e) {
+        var { type } = e.currentTarget.dataset
         this.setData({
-            pn:1,
-            tab:type
+            pn: 1,
+            tab: type
         });
-        this.fetch(type,1);
+        this.fetch(type, 1);
     },
-    fetchMore(){
-        var {tab,pn,fetching}=this.data;
-        wx.showLoading({
-            title: '加载中',
-        });
-        if(!fetching){
-            this.setData({fetching:true});
+    fetchMore() {
+        var { tab, pn, fetching } = this.data;
+        wx.showNavigationBarLoading();
+        if (!fetching) {
+            this.setData({ fetching: true });
             this.fetch();
         }
     },
-    fetch(){
-        var _this=this;
-        var {tab,pn,list}=this.data;
-        fetchNewsList(tab,pn).then((data)=>{
-            wx.hideLoading();
-            var fetchList=data.list;
-            if(fetchList.length==0){
+    fetch() {
+        var _this = this;
+        var { tab, pn, list } = this.data;
+        fetchNewsList(tab, pn).then(function(data){
+            var fetchList = data.list;
+            console.log(fetchList);
+            if (fetchList.length == 0) {
                 showToastError("没有更多了");
             }
-            var preList=list[tab];
-            list[tab]=preList.concat(fetchList);
+            var preList = list[tab];
+            list[tab] = preList.concat(fetchList);
             _this.setData({
                 list,
-                pn:pn+1,
-                fetching:false
+                pn: pn + 1,
+                fetching: false
             });
-        }).catch((err)=>{
-            showToastError(err);
-            wx.hideLoading();            
+            wx.hideNavigationBarLoading();
+        }).catch((err) => {
+            showToastError("网络错误");
+            wx.hideNavigationBarLoading();
         });
     }
 })

8a5ac32394f7731e93e2d5f6e80fc939bbb289ee
zhangguanyu02 <zhangguanyu02@meituan.com>
2017-04-14 22:37:17 +0800

    update

--- pages/personal/msg/msg.js
+++ pages/personal/msg/msg.js
@@ -1,32 +1,57 @@
-var {fetchMsg}=require('../../../utils/service');
-var {showToastError,showToastSuccess,checkMemoryBindUrp,navigateToLogin}=require('../../../utils/wxApp');
+var { fetchMsg } = require('../../../utils/service');
+var { showToastError, showToastSuccess, checkMemoryBindUrp, navigateToLogin } = require('../../../utils/wxApp');
 Page({
-	data: {
-		msgList:[],
-		editIndex:"",
-		
-	},
-	onLoad(){
-		this.fetch();
-	},
-	handleSlide(e){
-		var {index}=e.currentTarget.dataset;
-		console.log(e);
-	},
-	fetch(method="GET",msgId){
-		var _this=this;
-		fetchMsg(method,msgId).then(({pass,msgList,modify})=>{
-			if(pass){
-				if(method=="GET"){
-					_this.setData({msgList});
-				}else if(method=="DELETE"){
-					showToastSuccess("已删除");
-				}
-			}else{
-				navigateToLogin();
+    data: {
+        msgList: [],
+        editIndex: "",
+        startX: "",
+		move:[]
+    },
+    onLoad() {
+        this.fetch();
+    },
+    handleTouchStart(e) {
+        var { index } = e.currentTarget.dataset;
+        if (e.touches.length == 1) {
+            var touches = e.touches[0];
+            var { pageX } = touches;
+            this.setData({ startX: pageX, editIndex: index })
+        }
+    },
+    handleTouchEnd(e) {
+        this.setData({startX:""})
+    },
+    handleTouchMove(e) {
+        var { index } = e.currentTarget.dataset;
+		if(e.changedTouches.length==1){
+			var touches=e.changedTouches[0];
+			var {pageX}=touches;
+			var {startX}=this.data;
+			if(startX-pageX>60){
+				var {move}=this.data;
+				move[index]=true;
+				this.setData({move});
+			}else if(pageX-startX>60){
+				var {move}=this.data;
+				move[index]=false;
+				this.setData({move});
 			}
-		}).catch((err)=>{
-			showToastError(err);
-		})
-	}
+		}
+    },
+    fetch(method = "GET", msgId) {
+        var _this = this;
+        fetchMsg(method, msgId).then(({ pass, msgList, modify }) => {
+            if (pass) {
+                if (method == "GET") {
+                    _this.setData({ msgList });
+                } else if (method == "DELETE") {
+                    showToastSuccess("已删除");
+                }
+            } else {
+                navigateToLogin();
+            }
+        }).catch((err) => {
+            showToastError(err);
+        })
+    }
 })
